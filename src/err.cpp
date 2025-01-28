a7f2288a7d4e873531ca8bd3f99e7cd28cb2b57d
ALJLBL <aljlbl1998@gmail.com>
2025-01-28 16:06:12 +0900

    エラーチェック機能を改善し、エラーチェックフラグを追加。受信データの異常時にCANメッセージを送信する条件を修正。

--- src/err.cpp
+++ src/err.cpp
@@ -3,6 +3,11 @@
 #include <can.h>
 
 void err_check(unsigned long time_recv, unsigned char* data_recv) {
+    if (!g_err_check_flag) {
+        return;
+    }
+    g_err_check_flag = false;
+
     bool status_err_check = false;  // エラーチェックステータス
 
     // 2000ms以上受信がない場合、エラーステータスを2に設定
@@ -19,9 +24,13 @@ void err_check(unsigned long time_recv, unsigned char* data_recv) {
     }
 
     // 受信データが0,1,2,3以外の場合、受信データを3に設定
-    status_err_check = (*data_recv != 0 && *data_recv != 1 && *data_recv != 2 && *data_recv != 3);
+    status_err_check = (*data_recv != 0 && *data_recv != 1 && *data_recv != 2 && *data_recv != 3 && *data_recv != 4);
+    // if (*data_recv == 4) {
+    //     canInit();
+    // }
     if (status_err_check) {
         *data_recv = 3;
         can_send(0x111, 1);
     }
+    
 }
