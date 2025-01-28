3037329dfc476cce2d1f5cd7a0a9f1e5e166ede6
ALJLBL <aljlbl1998@gmail.com>
2025-01-28 09:56:36 +0900

    受信フラグの型をboolに変更し、送信フラグとエラーステータスを追加。受信関数に位置引数を追加。

--- src/can.h
+++ src/can.h
@@ -8,14 +8,16 @@
 //===========================================================//
 extern MCP_CAN CAN;
 extern int pinCan;
-extern int g_recvFlag;
+extern bool g_recvFlag;     // 受信フラグ
+extern bool g_send_flag;    // 送信フラグ
+extern uint8_t g_err_status;    // エラーステータス
 
 //===========================================================//
 //==================関数プロトタイプ宣言=======================//
 //===========================================================//
 void canInit();
 void recvHandler();
-bool can_recv(unsigned long* id, unsigned char* data, unsigned long* time);
+bool can_recv(unsigned long* id, unsigned char* data, unsigned long* time, uint8_t position);
 void can_send(unsigned long id, unsigned char data);
 
 #endif // CAN_H
