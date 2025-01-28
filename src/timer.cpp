117a1b58233cecf6751836958db5c9fffa5c0f52
ALJLBL <aljlbl1998@gmail.com>
2025-01-28 09:56:20 +0900

    タイマー機能にフラグを追加し、タイマー処理の状態を管理するように修正

--- src/timer.cpp
+++ src/timer.cpp
@@ -1,10 +1,18 @@
 #include <timer.h>
 
+bool g_timer_flag = false;    // タイマフラグの定義
+bool g_flash_flag = false;    // フラッシュフラグの定義
+bool g_send_flag = false;    // 送信フラグの定義
+bool g_lcd_flag = false;    // LCDフラグの定義
+
 void timerInit() {
     MsTimer2::set(500, timerHandler);
     MsTimer2::start();
 }
 
 void timerHandler() {
-    g_timer_flag = 1;
+    g_timer_flag = true;
+    g_flash_flag = !g_flash_flag;
+    g_send_flag = true;
+    g_lcd_flag = true;
 }
