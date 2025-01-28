36e532d3fdd547a64ea8a7e549e9c80b75a047d5
ALJLBL <aljlbl1998@gmail.com>
2025-01-28 12:59:46 +0900

    CAN受信機能を改善し、特定のIDからの受信データを保存する機能を追加。受信確認フレームの送信を統合。

--- src/can.cpp
+++ src/can.cpp
@@ -2,6 +2,7 @@
 
 MCP_CAN CAN(10);
 int pinCan = 2;
+int pinLight = A0;
 bool g_recvFlag = 0;
 bool g_recvFlag_err = 0;
 uint8_t g_err_status = 0;
@@ -35,28 +36,30 @@ void recvHandler() {
 // 受信したデータを保存
 bool can_recv(unsigned long* id, unsigned char* data, unsigned long* time, uint8_t position) {
     bool err_flag = false;  // エラーフラグ
-    
-    // Check if there's a message waiting, even if flag isn't set
+    // 受信フラグが立っている場合、CAN受信
     if(digitalRead(pinCan) == LOW || g_recvFlag) {
+        g_recvFlag = 0;
+
         unsigned char data_recv[8]; // 8個のunsigned char型の要素を持つ配列を定義
         unsigned char len = 0;      // 受信データの長さを格納
-        
-        // Always clear the flag before reading
-        g_recvFlag = 0;
-        
-        // Check for messages until the buffer is empty
+
+        // 受信データがある場合、CAN受信
         while(CAN.checkReceive() == CAN_MSGAVAIL) {
-            if (CAN.readMsgBuf(id, &len, data_recv) == CAN_OK) {
-                // 受信データを保存
-                *data = data_recv[position];
-                // 受信時刻を保存
-                *time = millis();
-                // 受信データをシリアルモニタに表示
-                Serial.print("Recv from 0x");
-                Serial.print(*id, HEX);
-                Serial.print(" : ");
-                Serial.println(*data);
-                err_flag = 0;
+            if ((CAN.readMsgBuf(id, &len, data_recv) == CAN_OK)) {
+                if (*id == 0x100) {
+                    // 受信データを保存
+                    *data = data_recv[position];
+                    // 受信確認フレームを送信
+                    can_send(0x104, 0);
+                    // 受信時刻を保存
+                    *time = millis();
+                    // 受信データをシリアルモニタに表示
+                    Serial.print("Recv from 0x");
+                    Serial.print(*id, HEX);
+                    Serial.print(" : ");
+                    Serial.println(*data);
+                    err_flag = 0;
+                }
             } else {
                 Serial.println("CAN recv Error !!!");
                 err_flag = 1;
@@ -72,15 +75,14 @@ void can_send(unsigned long id, unsigned char data) {
     // 送信するdataのサイズを確認
     uint8_t len = sizeof(data);;
     // 送信フラグが立っている場合、CAN送信
-    if (g_send_flag) {
-        g_send_flag = 0;
-        if(CAN.sendMsgBuf(id, len, &data) == CAN_OK) {
-            Serial.print("Send to 0x");
-            Serial.print(id, HEX);
-            Serial.print(" : ");
-            Serial.println(data);
-        } else {
-            Serial.println("Send to 0x100 Error !!!");
-        }
+    if(CAN.sendMsgBuf(id, len, &data) == CAN_OK) {
+        Serial.print("Send to 0x");
+        Serial.print(id, HEX);
+        Serial.print(" : ");
+        Serial.println(data);
+    } else {
+        Serial.println("Send to 0x");
+        Serial.print(id, HEX);
+        Serial.println(" Error !!!");
     }
 }
