850b1f44c2fa54ec7a1d30d8b129759b65ac9295
ALJLBL <aljlbl1998@gmail.com>
2025-01-28 12:59:51 +0900

    エラーチェック機能を改善し、受信がない場合にCANメッセージを送信する機能を追加。受信データの異常時にもCANメッセージを送信するように修正。

--- src/err.cpp
+++ src/err.cpp
@@ -1,5 +1,6 @@
 #include <err.h>
 #include <Arduino.h>
+#include <can.h>
 
 void err_check(unsigned long time_recv, unsigned char* data_recv) {
     bool status_err_check = false;  // エラーチェックステータス
@@ -7,6 +8,7 @@ void err_check(unsigned long time_recv, unsigned char* data_recv) {
     // 2000ms以上受信がない場合、エラーステータスを2に設定
     if (millis() - time_recv > 2000) {
         g_err_status = 2;
+        can_send(0x111, 2);
     }
 
     // エラーステータスが2の場合、受信があればエラーステータスを0に設定
@@ -20,5 +22,6 @@ void err_check(unsigned long time_recv, unsigned char* data_recv) {
     status_err_check = (*data_recv != 0 && *data_recv != 1 && *data_recv != 2 && *data_recv != 3);
     if (status_err_check) {
         *data_recv = 3;
+        can_send(0x111, 1);
     }
 }
