0fb8ff35da74138c02edb28cbfa54fc00bfc879a
ALJLBL <aljlbl1998@gmail.com>
2025-01-28 16:06:27 +0900

    エラーチェックフラグを追加し、タイマーハンドラ内でフラグを設定する処理を実装

--- src/timer.cpp
+++ src/timer.cpp
@@ -4,6 +4,7 @@ bool g_timer_flag = false;    // タイマフラグの定義
 bool g_flash_flag = false;    // フラッシュフラグの定義
 bool g_send_flag = false;    // 送信フラグの定義
 bool g_lcd_flag = false;    // LCDフラグの定義
+bool g_err_check_flag = false;    // エラーチェックフラグの定義
 
 void timerInit() {
     MsTimer2::set(500, timerHandler);
@@ -15,4 +16,5 @@ void timerHandler() {
     g_flash_flag = !g_flash_flag;
     g_send_flag = true;
     g_lcd_flag = true;
+    g_err_check_flag = true;
 }
--- src/timer.h
+++ src/timer.h
@@ -11,6 +11,7 @@ extern bool g_timer_flag;    // タイマフラグ
 extern bool g_flash_flag;    // フラッシュフラグ
 extern bool g_send_flag;    // 送信フラグ
 extern bool g_lcd_flag;    // LCDフラグ
+extern bool g_err_check_flag;    // エラーチェックフラグ
 
 //===========================================================//
 //==================関数プロトタイプ宣言=======================//
