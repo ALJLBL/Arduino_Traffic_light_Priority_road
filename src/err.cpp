fd5a7d0490aeb03e185ed6fc83bbc5e33ca67827
ALJLBL <aljlbl1998@gmail.com>
2025-01-29 11:28:45 +0900

    エラーチェック関数のコードを整理し、関数定義セクションを追加。受信がない場合のエラーステータス処理を改善し、受信データの設定を修正。

--- src/err.cpp
+++ src/err.cpp
@@ -2,6 +2,10 @@
 #include <Arduino.h>
 #include <can.h>
 
+
+//===========================================================//
+//========================関数定義============================//
+//===========================================================//
 void err_check(unsigned long time_recv, unsigned char* data_recv) {
     if (!g_err_check_flag) {
         return;
@@ -10,18 +14,23 @@ void err_check(unsigned long time_recv, unsigned char* data_recv) {
 
     bool status_err_check = false;  // エラーチェックステータス
 
-    // 2000ms以上受信がない場合、エラーステータスを2に設定
+    // 2000ms以上受信がない場合、受信データを4に設定 、CAN初期化、Cエラーコード送信
     if (millis() - time_recv > 2000) {
-        g_err_status = 2;
+        Serial.print("Last Recv Time : ");
+        Serial.println(millis() - time_recv);
+
+        *data_recv = 4;
+
+        canInit();
         can_send(0x111, 2);
     }
 
     // エラーステータスが2の場合、受信があればエラーステータスを0に設定
-    if (g_err_status == 2) {
-        if (millis() - time_recv <= 2000) {
-            g_err_status = 0;
-        }
-    }
+    // if (*data_recv == 2) {
+    //     if (millis() - time_recv <= 2000) {
+    //         *data_recv = 0;
+    //     }
+    // }
 
     // 受信データが0,1,2,3以外の場合、受信データを3に設定
     status_err_check = (*data_recv != 0 && *data_recv != 1 && *data_recv != 2 && *data_recv != 3 && *data_recv != 4);
